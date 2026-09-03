561659ac8752825c45ee26e376fc338bbb803d21
Nizar Tuanku <71693755+nizartuanku@users.noreply.github.com>
2026-09-03 16:59:00 +0700

    0.4.1 (5/7): version 0.4.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 
 // Version is the semantic version of this build. Overridden at build time with
 // -ldflags "-X github.com/nizartuanku/topolight/internal/version.Version=x.y.z".
-var Version = "0.4.0"
+var Version = "0.4.1"
 
 // Product is the human name.
 const Product = "TopoLight"
